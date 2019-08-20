eb653ec9c8a8d905b51a5c7135c6949b39d8a325
OXOYO <zmn2007.hi@163.com>
2019-08-20 09:59:33 +0800

    新增弹窗组件。

---
+++ src/global/components/Modal.vue
@@ -0,0 +1,21 @@
+/**
+* Created by OXOYO on 2018/3/30.
+*
+* 弹窗组件
+*/
+
+<style scoped lang="less" rel="stylesheet/less">
+  .xpe_modal {}
+</style>
+
+<template>
+  <div class="xpe_modal">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'XPEModal'
+  }
+</script>
